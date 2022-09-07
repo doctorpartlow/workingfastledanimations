234f08687e639321131722bc2bfd2a1a1dfe2b58
GregP-Navdna <104438530+GregP-Navdna@users.noreply.github.com>
2022-09-06 20:31:51 -0400

    flowfields

---
+++ matrix24x24esp32pin42flowfieldsboids/matrix24x24flowfieldboids.ino
@@ -0,0 +1,600 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#define LED_PIN 42    //this is for the esp32-s3 
+#define BRIGHTNESS 75 //it's blindingly bright at 255 or looks silly if you don't have enough power injected
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+const uint8_t kMatrixWidth = 24;
+const uint8_t kMatrixHeight = 24;
+#define WIDTH kMatrixWidth
+#define HEIGHT kMatrixHeight
+#define ROWS kMatrixWidth
+#define COLS kMatrixHeight
+#define LED_ROWS kMatrixWidth
+#define LED_COLS kMatrixHeight
+CRGB leds[kMatrixWidth * kMatrixHeight];
+const bool kMatrixSerpentineLayout = true;
+#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
+
+template <class T>
+class vec2 {
+public:
+	T x, y;
+	
+	vec2() :x(0), y(0) {}
+	vec2(T x, T y) : x(x), y(y) {}
+	vec2(const vec2& v) : x(v.x), y(v.y) {}
+	
+	vec2& operator=(const vec2& v) {
+		x = v.x;
+		y = v.y;
+		return *this;
+	}
+    bool operator==(vec2& v) {
+        return x == v.x && y == v.y;
+    }
+
+    bool operator!=(vec2& v) {
+        return !(x == y);
+    }
+	
+	vec2 operator+(vec2& v) {
+		return vec2(x + v.x, y + v.y);
+	}
+	vec2 operator-(vec2& v) {
+		return vec2(x - v.x, y - v.y);
+	}
+	
+	vec2& operator+=(vec2& v) {
+		x += v.x;
+		y += v.y;
+		return *this;
+	}
+	vec2& operator-=(vec2& v) {
+		x -= v.x;
+		y -= v.y;
+		return *this;
+	}	
+	vec2 operator+(double s) {
+		return vec2(x + s, y + s);
+	}
+	vec2 operator-(double s) {
+		return vec2(x - s, y - s);
+	}
+	vec2 operator*(double s) {
+		return vec2(x * s, y * s);
+	}
+	vec2 operator/(double s) {
+		return vec2(x / s, y / s);
+	}
+	
+	
+	vec2& operator+=(double s) {
+		x += s;
+		y += s;
+		return *this;
+	}
+	vec2& operator-=(double s) {
+		x -= s;
+		y -= s;
+		return *this;
+	}
+	vec2& operator*=(double s) {
+		x *= s;
+		y *= s;
+		return *this;
+	}
+	vec2& operator/=(double s) {
+		x /= s;
+		y /= s;
+		return *this;
+	}
+	
+	void set(T x, T y) {
+		this->x = x;
+		this->y = y;
+	}
+	
+	void rotate(double deg) {
+		double theta = deg / 180.0 * M_PI;
+		double c = cos(theta);
+		double s = sin(theta);
+		double tx = x * c - y * s;
+		double ty = x * s + y * c;
+		x = tx;
+		y = ty;
+	}
+	
+	vec2& normalize() {
+		if (length() == 0) return *this;
+		*this *= (1.0 / length());
+		return *this;
+	}
+	
+	float dist(vec2 v) const {
+		vec2 d(v.x - x, v.y - y);
+		return d.length();
+	}
+	float length() const {
+		return sqrt(x * x + y * y);
+	}
+	void truncate(double length) {
+		double angle = atan2f(y, x);
+		x = length * cos(angle);
+		y = length * sin(angle);
+	}
+	
+	vec2 ortho() const {
+		return vec2(y, -x);
+	}
+	
+	static float dot(vec2 v1, vec2 v2) {
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+	static float cross(vec2 v1, vec2 v2) {
+		return (v1.x * v2.y) - (v1.y * v2.x);
+	}
+
+  float mag() const {
+      return length();
+  }
+
+  float magSq() {
+        return (x * x + y * y);
+  }
+
+  void limit(float max) {
+    if (magSq() > max*max) {
+        normalize();
+        *this *= max;
+    }
+  }
+	
+};
+
+typedef vec2<float> PVector;
+typedef vec2<double> vec2d;
+//<-- end of the vector library
+
+ #define LED_PIN 42   //this is for the esp8266
+
+// #define BRIGHTNESS 200 //it's blindingly bright at 255 or looks silly if you don't have enough power injected
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+// #define NUM_LEDS 576
+//CRGB leds[NUM_LEDS];
+#define NUM_PARTICLES 40  //set this to the number of particles. the varialbe describes what it's supposed to be. it works with 50 but it's a little slow. on an esp32 it looks pretty nice at that number 15 is a safe number
+// const uint8_t kMatrixWidth = 24;
+// const uint8_t kMatrixHeight = 24;
+ #define WIDTH kMatrixWidth
+ #define HEIGHT kMatrixHeight
+ #define ROWS kMatrixWidth
+ #define COLS kMatrixHeight
+ #define LED_ROWS kMatrixWidth
+ #define LED_COLS kMatrixHeight
+ bool loadingFlag = true;
+ //CRGB leds[kMatrixWidth * kMatrixHeight];
+ //const bool kMatrixSerpentineLayout = true;
+ #define NUM_LEDS (kMatrixWidth * kMatrixHeight)
+ //#define enlargedOBJECT_MAX_COUNT WIDTH * 2
+// //#define SPEED_ADJ (float)NUM_LEDS / 512
+// //--> here's some pixel color placement stuff that's very useful. I don't konw what that russian means but I'm sure it has something to do with the calculations. 
+// //I didn't run it through a translater because it works so unless you want to konw what that russian says, then just imagine that it is something about the calculations (I'm assuming it is but it could be profanity. I doubt it though.)
+
+uint16_t XY( uint8_t x, uint8_t y)
+{
+  uint16_t i;
+  if( kMatrixSerpentineLayout == false) {
+    i = (y * kMatrixWidth) + x;
+  }
+  if( kMatrixSerpentineLayout == true) {
+    if( y & 0x01) {
+      // Odd rows run backwards
+      uint8_t reverseX = (kMatrixWidth - 1) - x;
+      i = (y * kMatrixWidth) + reverseX;
+    } else {
+      // Even rows run forwards
+      i = (y * kMatrixWidth) + x;
+    }
+  }
+  return i;
+}
+
+uint32_t getPixColor(uint32_t thisSegm)
+{
+  uint32_t thisPixel = thisSegm;// * SEGMENTS;
+  if (thisPixel > NUM_LEDS - 1) return 0;
+  return (((uint32_t)leds[thisPixel].r << 16) | ((uint32_t)leds[thisPixel].g << 8 ) | (uint32_t)leds[thisPixel].b); // а почему не просто return (leds[thisPixel])?
+}
+
+// функция получения цвета пикселя в матрице по его координатам
+uint32_t getPixColorXY(uint8_t x, uint8_t y)
+{
+  return getPixColor(XY(x, y));
+}
+
+// функция отрисовки точки по координатам X Y
+void drawPixelXY(int8_t x, int8_t y, CRGB color)
+{
+  if (x < 0 || x > (WIDTH - 1) || y < 0 || y > (HEIGHT - 1)) return;
+  uint32_t thisPixel = XY((uint8_t)x, (uint8_t)y);// * SEGMENTS;
+  //for (uint8_t i = 0; i < SEGMENTS; i++)
+  //{
+    leds[thisPixel] = color;
+  //}
+}
+void drawPixelXYF(float x, float y, CRGB color) //, uint8_t darklevel = 0U)
+{
+//  if (x<0 || y<0) return; //не похоже, чтобы отрицательные значения хоть как-нибудь учитывались тут // зато с этой строчкой пропадает нижний ряд
+  // extract the fractional parts and derive their inverses
+  uint8_t xx = (x - (int)x) * 255, yy = (y - (int)y) * 255, ix = 255 - xx, iy = 255 - yy;
+  // calculate the intensities for each affected pixel
+  #define WU_WEIGHT(a,b) ((uint8_t) (((a)*(b)+(a)+(b))>>8))
+  uint8_t wu[4] = {WU_WEIGHT(ix, iy), WU_WEIGHT(xx, iy),
+                   WU_WEIGHT(ix, yy), WU_WEIGHT(xx, yy)};
+  // multiply the intensities by the colour, and saturating-add them to the pixels
+  for (uint8_t i = 0; i < 4; i++) {
+    int16_t xn = x + (i & 1), yn = y + ((i >> 1) & 1);
+    CRGB clr = getPixColorXY(xn, yn);
+    clr.r = qadd8(clr.r, (color.r * wu[i]) >> 8);
+    clr.g = qadd8(clr.g, (color.g * wu[i]) >> 8);
+    clr.b = qadd8(clr.b, (color.b * wu[i]) >> 8);
+//if (darklevel) drawPixelXY(xn, yn, makeDarker(clr, darklevel));
+//else
+    drawPixelXY(xn, yn, clr);
+  }
+}
+// //-----------------> end of pixel placement stuff
+
+// //------------boid stuff----------->
+class Boid {
+  public:
+
+    PVector location;
+    PVector velocity;
+    PVector acceleration;
+    float maxforce;    // Maximum steering force
+    float maxspeed;    // Maximum speed
+    int hue;
+    float desiredseparation = 5;
+    float neighbordist = 10;
+    byte colorIndex = 0;
+    float mass;
+
+    boolean enabled = true;
+
+    Boid() {}
+
+    Boid(float x, float y) {
+      acceleration = PVector(0, 0);
+      velocity = PVector(randomf(), randomf());
+      location = PVector(x, y);
+      maxspeed = 1.5;
+      maxforce = 0.05;
+      mass = random(1.0,2.5);
+      hue = random(40,255);
+    }
+
+    static float randomf() {
+      return mapfloat(random(0, 255), 0, 255, -.5, .5);
+    }
+
+    static float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
+      return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+    void run(Boid boids [], uint8_t boidCount) {
+      flock(boids, boidCount);
+      update();
+      // wrapAroundBorders();
+      // render();
+    }
+
+    // Method to update location
+    void update() {
+      // Update velocity
+      velocity += acceleration;
+      // Limit speed
+      velocity.limit(maxspeed);
+      location += velocity;
+      // Reset acceleration to 0 each cycle
+      acceleration *= 0;
+    }
+
+    void applyForce(PVector force) {
+      // We could add mass here if we want A = F / M
+      acceleration += force;
+    }
+
+    void repelForce(PVector obstacle, float radius) {
+      //Force that drives boid away from obstacle.
+
+      PVector futPos = location + velocity; //Calculate future position for more effective behavior.
+      PVector dist = obstacle - futPos;
+      float d = dist.mag();
+
+      if (d <= radius) {
+        PVector repelVec = location - obstacle;
+        repelVec.normalize();
+        if (d != 0) { //Don't divide by zero.
+          // float scale = 1.0 / d; //The closer to the obstacle, the stronger the force.
+          repelVec.normalize();
+          repelVec *= (maxforce * 7);
+          if (repelVec.mag() < 0) { //Don't let the boids turn around to avoid the obstacle.
+            repelVec.y = 0;
+          }
+        }
+        applyForce(repelVec);
+      }
+    }
+
+    // We accumulate a new acceleration each time based on three rules
+    void flock(Boid boids [], uint8_t boidCount) {
+      PVector sep = separate(boids, boidCount);   // Separation
+      PVector ali = align(boids, boidCount);      // Alignment
+      PVector coh = cohesion(boids, boidCount);   // Cohesion
+      // Arbitrarily weight these forces
+      sep *= 1.5;
+      ali *= 1.0;
+      coh *= 1.0;
+      // Add the force vectors to acceleration
+      applyForce(sep);
+      applyForce(ali);
+      applyForce(coh);
+    }
+
+    // Separation
+    // Method checks for nearby boids and steers away
+    PVector separate(Boid boids [], uint8_t boidCount) {
+      PVector steer = PVector(0, 0);
+      int count = 0;
+      // For every boid in the system, check if it's too close
+      for (int i = 0; i < boidCount; i++) {
+        Boid other = boids[i];
+        if (!other.enabled)
+          continue;
+        float d = location.dist(other.location);
+        // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+        if ((d > 0) && (d < desiredseparation)) {
+          // Calculate vector pointing away from neighbor
+          PVector diff = location - other.location;
+          diff.normalize();
+          diff /= d;        // Weight by distance
+          steer += diff;
+          count++;            // Keep track of how many
+        }
+      }
+      // Average -- divide by how many
+      if (count > 0) {
+        steer /= (float) count;
+      }
+
+      // As long as the vector is greater than 0
+      if (steer.mag() > 0) {
+        // Implement Reynolds: Steering = Desired - Velocity
+        steer.normalize();
+        steer *= maxspeed;
+        steer -= velocity;
+        steer.limit(maxforce);
+      }
+      return steer;
+    }
+
+    // Alignment
+    // For every nearby boid in the system, calculate the average velocity
+    PVector align(Boid boids [], uint8_t boidCount) {
+      PVector sum = PVector(0, 0);
+      int count = 0;
+      for (int i = 0; i < boidCount; i++) {
+        Boid other = boids[i];
+        if (!other.enabled)
+          continue;
+        float d = location.dist(other.location);
+        if ((d > 0) && (d < neighbordist)) {
+          sum += other.velocity;
+          count++;
+        }
+      }
+      if (count > 0) {
+        sum /= (float) count;
+        sum.normalize();
+        sum *= maxspeed;
+        PVector steer = sum - velocity;
+        steer.limit(maxforce);
+        return steer;
+      }
+      else {
+        return PVector(0, 0);
+      }
+    }
+
+    // Cohesion
+    // For the average location (i.e. center) of all nearby boids, calculate steering vector towards that location
+    PVector cohesion(Boid boids [], uint8_t boidCount) {
+      PVector sum = PVector(0, 0);   // Start with empty vector to accumulate all locations
+      int count = 0;
+      for (int i = 0; i < boidCount; i++) {
+        Boid other = boids[i];
+        if (!other.enabled)
+          continue;
+        float d = location.dist(other.location);
+        if ((d > 0) && (d < neighbordist)) {
+          sum += other.location; // Add location
+          count++;
+        }
+      }
+      if (count > 0) {
+        sum /= count;
+        return seek(sum);  // Steer towards the location
+      }
+      else {
+        return PVector(0, 0);
+      }
+    }
+
+    // A method that calculates and applies a steering force towards a target
+    // STEER = DESIRED MINUS VELOCITY
+    PVector seek(PVector target) {
+      PVector desired = target - location;  // A vector pointing from the location to the target
+      // Normalize desired and scale to maximum speed
+      desired.normalize();
+      desired *= maxspeed;
+      // Steering = Desired minus Velocity
+      PVector steer = desired - velocity;
+      steer.limit(maxforce);  // Limit to maximum steering force
+      return steer;
+    }
+
+    // A method that calculates a steering force towards a target
+    // STEER = DESIRED MINUS VELOCITY
+    void arrive(PVector target) {
+      PVector desired = target - location;  // A vector pointing from the location to the target
+      float d = desired.mag();
+      // Normalize desired and scale with arbitrary damping within 100 pixels
+      desired.normalize();
+      if (d < 4) {
+        float m = map(d, 0, 100, 0, maxspeed);
+        desired *= m;
+      }
+      else {
+        desired *= maxspeed;
+      }
+
+      // Steering = Desired minus Velocity
+      PVector steer = desired - velocity;
+      steer.limit(maxforce);  // Limit to maximum steering force
+      applyForce(steer);
+      //Serial.println(d);
+    }
+
+    void wrapAroundBorders() {
+      if (location.x < 0) location.x = ROWS - 1;
+      if (location.y < 0) location.y = COLS - 1;
+      if (location.x >= ROWS) location.x = 0;
+      if (location.y >= COLS) location.y = 0;
+    }
+
+    void avoidBorders() {
+      PVector desired = velocity;
+
+      if (location.x < 8) desired = PVector(maxspeed, velocity.y);
+      if (location.x >= ROWS - 8) desired = PVector(-maxspeed, velocity.y);
+      if (location.y < 8) desired = PVector(velocity.x, maxspeed);
+      if (location.y >= COLS - 8) desired = PVector(velocity.x, -maxspeed);
+
+      if (desired != velocity) {
+        PVector steer = desired - velocity;
+        steer.limit(maxforce);
+        applyForce(steer);
+      }
+
+      if (location.x < 0) location.x = 0;
+      if (location.y < 0) location.y = 0;
+      if (location.x >= ROWS) location.x = ROWS - 1;
+      if (location.y >= COLS) location.y = COLS - 1;
+    }
+
+    bool bounceOffBorders(float bounce) {
+      bool bounced = false;
+
+      if (location.x >= ROWS) {
+        location.x = ROWS - 1;
+        velocity.x *= -bounce;
+        bounced = true;
+      }
+      else if (location.x < 0) {
+        location.x = 0;
+        velocity.x *= -bounce;
+        bounced = true;
+      }
+
+      if (location.y >= COLS) {
+        location.y = COLS - 1;
+        velocity.y *= -bounce;
+        bounced = true;
+      }
+      else if (location.y < 0) {
+        location.y = 0;
+        velocity.y *= -bounce;
+        bounced = true;
+      }
+
+      return bounced;
+    }
+
+    void render() {
+
+    }
+};
+
+
+
+
+//     byte hue = 0;
+
+
+ Boid boids[NUM_PARTICLES];    //this makes the boids
+     uint16_t x;
+     uint16_t y;
+     uint16_t z;
+
+    uint16_t speed = 2;
+     uint16_t scale = 75;
+
+     static const int count = 40;
+
+        void start() {
+      x = random16();
+      y = random16();
+      z = random16();
+
+      for (int i = 0; i < count; i++) {
+        boids[i] = Boid(random(COLS), 0);
+      }
+    }
+
+void draw(){
+    if (loadingFlag)
+  {
+    loadingFlag = false;
+    
+    start();     
+   }
+ for (int i = 0; i < count; i++) {
+        Boid * boid = &boids[i];
+
+        int ioffset = scale * boid->location.x;
+        int joffset = scale * boid->location.y;
+
+        byte angle = inoise8(x + ioffset, y + joffset, z);
+
+        boid->velocity.x = (float) sin8(angle) * 0.0078125 - 1.0;
+        boid->velocity.y = -((float)cos8(angle) * 0.0078125 - 1.0);
+        boid->update();
+
+          drawPixelXYF(boid->location.x, boid->location.y, ColorFromPalette(ForestColors_p,boid->hue,255,LINEARBLEND));            
+        if (boid->location.x < 0 || boid->location.x >= ROWS ||
+            boid->location.y < 0 || boid->location.y >= COLS) {
+          boid->location.x = random(COLS);
+          boid->location.y = 0;
+        }
+      }
+      fadeToBlackBy(leds, NUM_LEDS, 64);
+
+
+      x += speed;
+      y += speed;
+      z += speed;
+}
+
+void setup()
+{
+  delay(3000);
+  LEDS.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  LEDS.setBrightness(BRIGHTNESS);
+}
+
+void loop(){
+draw();
+LEDS.show();
+}
