aafd7b7b251c275cec72b78850a9ca97bbcad511
GregP-Navdna <104438530+GregP-Navdna@users.noreply.github.com>
2024-03-10 18:59:37 -0400

    03102024 1900 boids update

---
+++ matrix24x24Esp32S3BoidsInViewport.ino
@@ -0,0 +1,157 @@
+template <class T>
+class vec2 {
+public:
+	T x, y;
+	
+	vec2() :x(0), y(0) {}
+	vec2(T x, T y) : x(x), y(y) {}
+	vec2(const vec2& v) : x(v.x), y(v.y) {}
+	
+	vec2& operator=(const vec2& v) {
+		x = v.x;
+		y = v.y;
+		return *this;
+	}
+    bool operator==(vec2& v) {
+        return x == v.x && y == v.y;
+    }
+
+    bool operator!=(vec2& v) {
+        return !(x == y);
+    }
+	
+	vec2 operator+(vec2& v) {
+		return vec2(x + v.x, y + v.y);
+	}
+	vec2 operator-(vec2& v) {
+		return vec2(x - v.x, y - v.y);
+	}	
+	vec2& operator+=(vec2& v) {
+		x += v.x;
+		y += v.y;
+		return *this;
+	}
+	vec2& operator-=(vec2& v) {
+		x -= v.x;
+		y -= v.y;
+		return *this;
+	}	
+	vec2 operator+(double s) {
+		return vec2(x + s, y + s);
+	}
+	vec2 operator-(double s) {
+		return vec2(x - s, y - s);
+	}
+	vec2 operator*(double s) {
+		return vec2(x * s, y * s);
+	}
+	vec2 operator/(double s) {
+		return vec2(x / s, y / s);
+	}
+	vec2& operator+=(double s) {
+		x += s;
+		y += s;
+		return *this;
+	}
+	vec2& operator-=(double s) {
+		x -= s;
+		y -= s;
+		return *this;
+	}
+	vec2& operator*=(double s) {
+		x *= s;
+		y *= s;
+		return *this;
+	}
+	vec2& operator/=(double s) {
+		x /= s;
+		y /= s;
+		return *this;
+	}
+	
+	void set(T x, T y) {
+		this->x = x;
+		this->y = y;
+	}
+	
+	void rotate(double deg) {
+		double theta = deg / 180.0 * M_PI;
+		double c = cos(theta);
+		double s = sin(theta);
+		double tx = x * c - y * s;
+		double ty = x * s + y * c;
+		x = tx;
+		y = ty;
+	}
+//this is the fast inverse square root. 
+  float Q_rsqrt( float number )
+  {
+        long i;
+        float x2, y;
+        const float threehalfs = 1.5F;
+ 
+        x2 = number * 0.5F;
+        y  = number;
+        i  = * ( long * ) &y;                       // evil floating point bit level hacking
+        i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+        y  = * ( float * ) &i;
+        y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+ 
+        return y;
+}
+	float length() const {
+		return sqrt(x * x + y * y);
+	}
+	//your processor likes this way of normalizing a lot more than the other way
+  //the fast inverse square root approximates the result using far less taxing calculations
+  //increasing over all perofrmance significantly 
+  //^^^ it's the function above this one. 
+
+  //verdict is the normal 1/sqrt shows no noticable slow down and it loioks better. so oh well. 
+	vec2& normalize() {
+		if (length() == 0) return *this;
+		*this *= (1.0 / length());// Q_rsqrt(x * x + y * y); // --> this is the slow way to normalize  (1.0 / length());
+		return *this;
+	}
+	
+	float dist(vec2 v) const {
+		vec2 d(v.x - x, v.y - y);
+		return d.length();
+	}
+
+
+	void truncate(double length) {
+		double angle = atan2f(y, x);
+		x = length * cos(angle);
+		y = length * sin(angle);
+	}
+	
+	vec2 ortho() const {
+		return vec2(y, -x);
+	}
+	
+	static float dot(vec2 v1, vec2 v2) {
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+	static float cross(vec2 v1, vec2 v2) {
+		return (v1.x * v2.y) - (v1.y * v2.x);
+	}
+
+  float mag() const {
+      return length();
+  }
+
+  float magSq() {
+        return (x * x + y * y);
+  }
+
+  void limit(float max) {
+    if (magSq() > max*max) {
+        normalize();
+        *this *= max;
+    }
+  }	
+};
+
+typedef vec2<float> PVector;
+typedef vec2<double> vec2d;
